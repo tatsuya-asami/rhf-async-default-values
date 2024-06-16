831523f193da27ce6a2116a07eab9cfd16af6e8c
tatsuya-asami <asami.tatsuya@commmune.jp>
2024-06-17 00:31:37 +0900

    feat: Add lazy loading for images and mock API response

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import useSWRMutation from "swr/mutation";
 import "./App.css";
+import useSWR from "swr";
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 async function sendRequest(
   url: string,
@@ -12,6 +15,10 @@ async function sendRequest(
 }
 
 function App() {
+  const { data } = useSWR("https://example.com/user", fetcher);
+  const { data: data2 } = useSWR("https://example.com/user", fetcher);
+  const { data: data3 } = useSWR("https://example.com/user", fetcher);
+
   const { trigger, isMutating } = useSWRMutation(
     "https://example.com/user",
     sendRequest
@@ -19,8 +26,11 @@ function App() {
 
   return (
     <div>
-      {isMutating ? "Loading..." : "Not loading"}
+      <div>data:{data && data.firstName}</div>
+      <div>data2:{data2 && data2.firstName}</div>
+      <div>data3:{data3 && data3.firstName}</div>
       <button onClick={() => trigger({ username: "sample" })}>Click me</button>
+      <div>{isMutating ? "Loading..." : "Not loading"}</div>
     </div>
   );
 }
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,6 +1,14 @@
 import { http, HttpResponse, delay } from "msw";
 
 export const handlers = [
+  http.get("https://example.com/user", async () => {
+    await delay(1500);
+    return HttpResponse.json({
+      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
+      firstName: "John",
+      lastName: "Maverick",
+    });
+  }),
   http.post("https://example.com/user", async () => {
     await delay(1500);
     return HttpResponse.json({
