c1a93fa3b63a736aaf00331656b3e97f2109c44a
tatsuya-asami <asami.tatsuya@commmune.jp>
2024-06-17 00:21:10 +0900

    feat: Add button to trigger post request
    The code changes introduce a button in the App component that allows users to trigger a post request. This enhances the functionality of the application by enabling user-server interaction.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
+import useSWRMutation from "swr/mutation";
 import "./App.css";
 
+async function sendRequest(
+  url: string,
+  { arg }: { arg: { username: string } }
+) {
+  return fetch(url, {
+    method: "POST",
+    body: JSON.stringify(arg),
+  }).then((res) => res.json());
+}
+
 function App() {
-  const postRequest = async () => {
-    console.log("post request");
-  };
+  const { trigger, isMutating } = useSWRMutation(
+    "https://example.com/user",
+    sendRequest
+  );
 
   return (
     <div>
-      <button onClick={postRequest}>Click me</button>
+      {isMutating ? "Loading..." : "Not loading"}
+      <button onClick={() => trigger({ username: "sample" })}>Click me</button>
     </div>
   );
 }
