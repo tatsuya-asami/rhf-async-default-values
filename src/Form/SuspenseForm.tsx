31d8a965130466a3db32a7c444d6d6a41278ce83
tatsuya-asami <asami.tatsuya@commmune.jp>
2024-06-30 15:30:48 +0900

    refactor: Update SuspenseForm inputs to be disabled during loading

--- src/Form/SuspenseForm.tsx
+++ src/Form/SuspenseForm.tsx
@@ -3,9 +3,9 @@ import { useFetchUserWithSuspense } from "../api";
 import { FormProps } from "../types";
 
 export const SuspenseForm = ({ outOfForm, description }: FormProps) => {
-  const { data } = useFetchUserWithSuspense();
+  const { data, isValidating } = useFetchUserWithSuspense();
   const methods = useForm({
-    defaultValues: {
+    values: {
       firstName: data.firstName,
       lastName: data.lastName,
       description,
@@ -20,9 +20,9 @@ export const SuspenseForm = ({ outOfForm, description }: FormProps) => {
           console.log(value);
         })}
       >
-        <input {...methods.register("firstName")} />
-        <input {...methods.register("lastName")} />
-        <input {...methods.register("description")} />
+        <input {...methods.register("firstName")} disabled={isValidating} />
+        <input {...methods.register("lastName")} disabled={isValidating} />
+        <input {...methods.register("description")} disabled={isValidating} />
         <button type="submit">Submit</button>
         <button type="button" onClick={() => methods.reset()}>
           Reset
